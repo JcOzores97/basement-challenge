b928e04f166eec9af569eacd5613d197ce0c4e3c
Juan Cruz Ozores <juanozores97@gmail.com>
2021-11-12 12:49:57 -0300

    add Product type

--- product/types.ts
+++ product/types.ts
@@ -1,1 +1,12 @@
-export interface Product {}
+type size = "S"| "M" |"L"| "XL";
+type sizeStock = {size:size, stock: number};
+
+export interface Product {
+    id: number,
+    name: string,
+    descriptionName: string,
+    stockBySize:sizeStock[],
+    priceInDollars:number,
+    imageSrc: string
+    imageAltText: string
+}
