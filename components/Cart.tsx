1f80a9b73fac2f8c2a845ef538155417a2379288
Juan Cruz Ozores <juanozores97@gmail.com>
2021-12-31 18:43:13 -0300

    add Radix Popover in Cart

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,27 +1,41 @@
 import styles from '../styles/Cart.module.css';
+import * as Popover from '@radix-ui/react-popover';
+import { useContext } from 'react';
+import { PurchaseContext } from '../context/purchase';
 type ComponentProps = {
-    onClose:( ev: React.MouseEvent<HTMLButtonElement>) => void
  };
  
 
-const Cart= ({onClose}:ComponentProps)=>{
+const Cart = (props:ComponentProps)=>{
+    const {state:{items}} = useContext(PurchaseContext);
 
     return(
-        <div className={styles.container}>
-            <button onClick={onClose} className={styles.closeButton}>CLOSE</button>
-            <h2>YOUR <span>CART</span></h2>
-            <div className={styles.itemsContainer}>
-            </div>
-            <div className={styles.total}>
-                <p>
-                    TOTAL
-                </p>
-                <p>
-                    $37,50 
-                </p>
-            </div>
-            <button className={styles.checkout}>CHECKOUT</button>
-        </div>
+        
+        <Popover.Root >
+           <Popover.Trigger className={styles.trigger} >
+                 CART ({items.length})
+            </Popover.Trigger> 
+            <Popover.Content className={styles.content} sideOffset={-52} alignOffset={-10}>
+                <Popover.Close className={styles.closeButton}> 
+                    Close
+                </Popover.Close >
+                <h2>YOUR <span>CART</span></h2>
+                <div className={styles.itemsContainer}>
+                </div>
+                <div className={styles.totalContainer}>
+                    <div className={styles.total}>
+                        <p>
+                            TOTAL
+                        </p>
+                        <p>
+                            $37,50 
+                        </p>
+                    </div>
+                    <button className={styles.checkout}>CHECKOUT</button>         
+                </div>
+            </Popover.Content>
+        </Popover.Root>
+      
     )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,44 +4,35 @@ import styles from '../styles/Home.module.css'
 import path from "path"
 import { promises as fs } from 'fs'
 import { Product as ProductType } from '../product/types'
-import { useContext, useState } from 'react';
 import Product from '../components/Product'
 import Cart from '../components/Cart'
-import { PurchaseContext } from '../context/purchase'
 
 
 
 const Home: NextPage = ({ products }: InferGetStaticPropsType<typeof getStaticProps>) => {
 
-  const {state: {items}} = useContext(PurchaseContext);
-  const [showCart, setShowCart] = useState(false);
 
   return (
-    <div className={showCart? styles.containerOnCartOpen : styles.containerOnCartClose}>
+    <div className={styles.container}>
       <Head>
-        <title>Basement challenge</title>
-        <meta name="description" content="Basement challenge developed in nextjs" />
-        <link rel="icon" href="/favicon.ico" />
+          <title>Basement challenge</title>
+          <meta name="description" content="Basement challenge developed in nextjs" />
+          <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <header className={styles.header}>
-        <img src="/leftAsterisk.svg" alt="asterisk" className={styles.leftAsterisk}/> 
-        <img src="/rightAsterisk.svg" alt="asterisk" className={styles.rightAsterisk}/> 
+          <img src="/leftAsterisk.svg" alt="asterisk" className={styles.leftAsterisk}/> 
+          <img src="/rightAsterisk.svg" alt="asterisk" className={styles.rightAsterisk}/> 
           <div className={styles.logo}>b.</div>
-            <img src="/hdLogo.svg" alt="hd-4k logo" className={styles.hdLogo}/> 
-            <div className={styles.cartContainer}> 
-              <button className={styles.openCartButton} onClick={(ev)=>{setShowCart(true)}}>CART ({items.length})</button>
-              {
-                showCart? <Cart onClose={(ev)=>setShowCart(false)} /> : null
-              }
-            </div> 
+          <img src="/hdLogo.svg" alt="hd-4k logo" className={styles.hdLogo}/> 
+          <Cart  />
           <img src="/header.svg" alt="basement supply header" className={styles.headerImg}/>
       </header>
       <div className={styles.marquee} >
-            <p>
+          <p>
             A man can´t have enough basement. swag
-            </p>
-          </div>
+          </p>
+      </div>
       <main className={styles.main}>
           <ul className={styles.listContainer}>
            { 
