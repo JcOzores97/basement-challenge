a793b4517ac6307822b86b01d5409fe3a7ce3ef4
Juan Cruz Ozores <juanozores97@gmail.com>
2022-01-01 20:17:35 -0300

    add Cart basic funcionality

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,5 +1,6 @@
 import styles from '../styles/Cart.module.css';
 import * as Popover from '@radix-ui/react-popover';
+import Image from 'next/dist/client/image';
 import { useContext } from 'react';
 import { PurchaseContext } from '../context/purchase';
 type ComponentProps = {
@@ -7,29 +8,58 @@ type ComponentProps = {
  
 
 const Cart = (props:ComponentProps)=>{
-    const {state:{items}} = useContext(PurchaseContext);
-
+    const {state:{items , total}, actions:{updateItemQuantity}} = useContext(PurchaseContext);
     return(
         
         <Popover.Root >
            <Popover.Trigger className={styles.trigger} >
-                 CART ({items.length})
+                 {`CART (${items.length})`}
             </Popover.Trigger> 
             <Popover.Content className={styles.content} sideOffset={-52} alignOffset={-10}>
                 <Popover.Close className={styles.closeButton}> 
                     Close
                 </Popover.Close >
-                <h2>YOUR <span>CART</span></h2>
+                <div className={styles.title}>
+                <h2>YOUR </h2>
+                <h2>CART</h2>
+                </div>
                 <div className={styles.itemsContainer}>
+                    {items.map((item)=>{
+                        return(
+                            <div key={item.id} className={styles.item}>
+                                <div className={styles.imageContainer}>
+                                    <Image src={item.imageSrc} alt={item.imageAltText} layout="responsive" width={880} height={1156}/>   
+                                </div>
+                                <div className={styles.infoContainer}>
+                                    <div className={styles.name}>{item.name}</div>
+                                    <div className={styles.descriptionName}>{item.descriptionName}</div>
+                                    <div className={styles.quantity}>
+                                        QUANTITY: 
+                                        <div className={styles.quantityUpdate}>
+                                            <button onClick={(ev)=>{updateItemQuantity(item.id,item.quantity - 1)}}>-</button>
+                                            {item.quantity}
+                                            <button onClick={(ev)=>{updateItemQuantity(item.id,item.quantity + 1)}}>+</button>
+                                        </div>
+                                    </div>
+                                    <div className={styles.size}>SIZE: 
+                                        {["S", "M", "L", "XL"].map((size)=>{
+                                           return (
+                                            <button key={size} >
+                                                {size}
+                                            </button>
+                                           ) 
+                                        })}
+                                    </div>
+                                    <div className={styles.price}>{`$ ${item.priceInDollars}`}</div>
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
                 <div className={styles.totalContainer}>
                     <div className={styles.total}>
-                        <p>
-                            TOTAL
-                        </p>
-                        <p>
-                            $37,50 
-                        </p>
+                        <p>TOTAL</p>
+                        <p> {`$ ${total}`}</p>
                     </div>
                     <button className={styles.checkout}>CHECKOUT</button>         
                 </div>
--- components/Product.tsx
+++ components/Product.tsx
@@ -1,6 +1,8 @@
 import { Product as ProductType } from '../product/types'
 import Image from "next/image";
 import styles from '../styles/Product.module.css';
+import { useContext } from 'react';
+import { PurchaseContext } from '../context/purchase';
 
 type ComponentProps = {
    product: ProductType
@@ -9,10 +11,12 @@ type ComponentProps = {
 
 const Product = ({product}:ComponentProps)=>{
 
+  const {actions:{addItem}} = useContext(PurchaseContext);
+
     return (
       <div className={styles.productContainer} >
         <div className={styles.product}>
-          <div className={styles.imageContainer} >
+          <div className={styles.imageContainer} onClick={()=>{addItem(product)}}>
               <Image src={product.imageSrc}  alt={product.imageAltText} layout="responsive" width={880} height={1156} />
           </div>
         </div>
