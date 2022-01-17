41f3323fc6ccdc94b39e37d377963e22e59e0a0a
Juan Cruz Ozores <juanozores97@gmail.com>
2022-01-17 18:26:24 -0300

    add svg on product click

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,23 +1,41 @@
 import { Product as ProductType } from '../product/types'
 import Image from "next/image";
 import styles from '../styles/Product.module.css';
-import { useContext } from 'react';
+import { useContext, useState, useRef } from 'react';
 import { PurchaseContext } from '../context/purchase';
+import { CSSTransition } from 'react-transition-group';
 
 type ComponentProps = {
    product: ProductType
 };
 
 
+
 const Product = ({product}:ComponentProps)=>{
-
+  const [showAnimation, setShowAnimation] = useState(false);
+  const nodeRef = useRef(null);
   const {actions:{addItem}} = useContext(PurchaseContext);
-
+  
     return (
       <div className={styles.productContainer} >
-        <div className={styles.product}>
+        <div className={styles.product} onClick={(ev)=>{setShowAnimation(true)}} >
           <div className={styles.imageContainer} onClick={()=>{addItem(product)}}>
-              <Image src={product.imageSrc}  alt={product.imageAltText} layout="responsive" width={880} height={1156} />
+              {showAnimation === false?<Image src={product.imageSrc}  alt={product.imageAltText} layout="responsive" width={880} height={1156} />:null}
+              <CSSTransition nodeRef={nodeRef} 
+                mountOnEnter
+                unmountOnExit 
+                in={showAnimation} 
+                timeout={500} 
+                onEntered={()=>{setShowAnimation(false)}}
+                classNames={{
+                  enter:styles.addToCartEnter,
+                  enterActive: styles.addToCartEnterActive,
+                  exit: styles.addToCartExit
+                }}>
+                  <div ref={nodeRef}>
+                    <img src="/addToCart.svg" alt="add to cart logo" className={styles.addToCartLogo}/> 
+                  </div>
+              </CSSTransition>
           </div>
         </div>
         <div className={styles.productInfo}>
@@ -28,6 +46,5 @@ const Product = ({product}:ComponentProps)=>{
     )
 }
    
-    
 
 export default Product;
